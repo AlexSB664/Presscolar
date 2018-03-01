dc7c2843ac5adf8c67d40701d85b997a30b1d8fb
Daniel Flores <danielflorestj@gmail.com>
2018-03-01 22:56:06 +0000

    bootstrap

--- alumnos/models.py
+++ alumnos/models.py
@@ -4,7 +4,11 @@ from padres.models import Tutor
 
 class alumnos(models.Model):
     alu_nombre = models.CharField(max_length = 400)
-    alu_tutores = models.ManyToManyField(Tutor, null = True)
+    alu_tutores = models.ManyToManyField(Tutor)
     
     def __str__(self):
         return self.alu_nombre
+    
+#class DiarioTrabajo(models.Models):
+#    DT_alumno = models.ForeignKey(alumnos, on_delete= models.CASCADE)
+#    DT_
