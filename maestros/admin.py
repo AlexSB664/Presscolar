709cfa0e3a7e2b3d099bb9d34de0300d483f9029
AlexSB664 <alexskullsoft@gmail.com>
2018-05-19 20:30:20 -0700

    Algunos bugs solucionados

--- maestros/admin.py
+++ maestros/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import grupos, DiarioTrabajo
+from .models import grupos, DiarioTrabajo, Evaluacion
 # Register your models here.
 
 admin.site.register(grupos)
 admin.site.register(DiarioTrabajo)
+admin.site.register(Evaluacion)
--- maestros/forms.py
+++ maestros/forms.py
@@ -2,9 +2,10 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.db import transaction
 from django.contrib.auth.models import User
+import datetime
 
 
 class StudentSignUpForm(UserCreationForm):#forms.Form UserCreationForm
     paterno = forms.CharField(label='Apellido Paterno:', widget=forms.TextInput(attrs={'class':'form-control','required' : 'True'}))
     materno = forms.CharField(label='Apellido Materno:', widget=forms.TextInput(attrs={'class':'form-control','required' : 'True'}))
-    nacimiento = forms.CharField(label='Fecha de nacimiento:', widget=forms.TextInput(attrs={'type':'date', 'class':'form-control'}))    
+    nacimiento = forms.CharField(label='Fecha de nacimiento:',initial=datetime.date.today, widget=forms.TextInput(attrs={'type':'date'}))    
